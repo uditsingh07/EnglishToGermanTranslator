ede4b316907fa7e248f2674dd0b8f659baa3a875
Udit <Udit@LAPTOP-UV8C7MD2>
2022-06-27 21:22:31 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Google.Cloud.Translation.V2;
+
+namespace EnglishToGermanTranslator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            Console.WriteLine("Enter Text in English");
+            text = Console.ReadLine();
+            var client = TranslationClient.CreateFromApiKey("Your GOOGLE Cloud Translation API Key");
+            var response = client.TranslateText(text, LanguageCodes.German, LanguageCodes.English);
+            Console.WriteLine("Text in German");
+            Console.WriteLine(response.TranslatedText);
+        }
+    }
+}
